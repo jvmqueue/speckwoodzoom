74d825bdee2c14e48e5ca877219af6b7ef5dfaf3
jvmqueue <wheels0090@yahoo.com>
2016-08-22 15:45:27 -0400

    define modal directive and open close functionality. This code has been tested in production

--- app/scripts/controllers/imagegallery.js
+++ app/scripts/controllers/imagegallery.js
@@ -11,35 +11,41 @@ angular.module('siteApp')
   .controller('ImagegalleryCtrl', ['$scope', function($scope){
     this.awesomeThings = ['HTML5 Boilerplate', 'AngularJS', 'Karma'];
     var _fnc = {
-        openCarousel:function(){
-            console.group('OPEN CAROUSEL');
-                console.log(':\t', 'Reached');
-               console.groupEnd(); 
+        openCarousel:function(paramIndex){ // close modal relative to index
+            var intIndex = paramIndex;
+            $scope.dataGallery[intIndex].show = !$scope.dataGallery[intIndex].show;
         }
     };
     $scope.dataGallery = [];
     $scope.dataGallery[0] = {path:'path to car image',
         description:'63 VW',
         year:'1963',
-        model:'Kombi'
+        model:'Kombi',
+        id:0,
+        show:false
     };
     $scope.dataGallery[1] = {path:'path to car image',
         description:'64 Chrysler',
         year:'1964',
-        model:'Pacifica'
+        model:'Pacifica',
+        id:1,
+        show:false
     };    
     $scope.dataGallery[2] = {path:'path to car image',
         description:'65 Dodge',
         year:'1964',
-        model:'Coronet'
+        model:'Coronet',
+        id:2,
+        show:false
     };        
     $scope.dataGallery[3] = {path:'path to car image',
         description:'65 Edsel',
         year:'1965',
-        model:'Echo'
+        model:'Echo',
+        id:3,
+        show:false
     };
     $scope.openCarousel = _fnc.openCarousel;
-
 
 
 
